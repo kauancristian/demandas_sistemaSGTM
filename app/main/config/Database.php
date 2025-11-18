4288bc854c283d7bc95b7c8a35ece520c7c434f9
Cássio <holandaseveriano@gmail.com>
2025-11-18 14:58:40 -0300

    feat-banco_atualizado

--- app/main/config/Database.php
+++ app/main/config/Database.php
@@ -1,40 +1,29 @@
 <?php
-//criando as constantes com os dados do banco
-define('HOST', 'localhost');
-define('DATABASE', 'sistema_de_demandas');
-define('USER', 'root');
-define('PASSWORD', '');
+class Database {
+    protected ?PDO $conn = null ;
 
-//criando a class
-class connect
-{
-    //atributos
-    protected $connect;
-
-    //metodos especiais
-    function __construct()
-    {
-        $this->connect_database();
+    protected function __construct() {
+        $this->connect();
     }
 
-    //metodos
-    function connect_database()
-    {
-
+    public function connect() {
         try {
-            //se conectando com o banco usando PDO
-            $this->connect = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USER, PASSWORD);
-        } catch (PDOException $e) {
-            echo "erro ao se conectar com o banco de dados" . $e->getMessage();
-            die();
-        }
-    }
+            $config = require __DIR__ . "/../../.env/config.php";
 
-    function query_fetch_assoc($sql)
-    {
-        //função para pegar os dados do banco de dados
-        $stmt = $this->connect->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $host = $config['local']['sistema_de_demandas']['host'];
+            $database = $config['local']['sistema_de_demandas']['banco'];
+            $user = $config['local']['sistema_de_demandas']['user'];
+            $password = $config['local']['sistema_de_demandas']['senha'];
+
+            $this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $user, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            error_log("Erro de conexão com banco: " . $e->getMessage());
+            $this->conn = null;
+            header('location:../views/windows/desconnect.php');
+            exit();
+        }
     }
 }
