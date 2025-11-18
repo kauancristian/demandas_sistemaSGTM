353c4448c103d5ff43a62732ae5e79bd214fcf52
Cássio <holandaseveriano@gmail.com>
2025-11-18 15:03:16 -0300

    feat-config_tables

---
+++ app/.env/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return $config = [
+    "local" => [
+        "host" => 'localhost',
+        "user" => 'root',
+        "senha" => '',
+        "banco" => 'sistema_de_demandas'
+    ]
+];
---
+++ app/.env/tables.php
@@ -0,0 +1,9 @@
+<?php
+
+return $tables = [
+    "sistema_de_demandas" => [
+        1 => "usuarios",
+        2 => "demandas",
+        3 => "usuario_demanda"
+    ]
+];
