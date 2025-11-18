d1e8bc593ecabd3455b787ddeb3965205de667b4
Cássio <holandaseveriano@gmail.com>
2025-11-18 16:38:27 -0300

    feat-models

---
+++ app/main/models/Usuario.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once __DIR__ . "../config/Database.php";
+
+class Usuario extends Database {
+    private string $table1;
+    private string $table2;
+    private string $table3;
+    private string $table4;
+    private string $table5;
+
+    public function __construct() {
+        parent::__construct();
+
+        $table = require __DIR__ . "/../../.env/tables.php";
+
+        $this->table1 = $table['sistema_de_demandas'][1];
+        $this->table2 = $table['sistema_de_demandas'][2];
+        $this->table3 = $table['sistema_de_demandas'][3];
+
+    }
+
+    public function cadastro(string $nome, string $email, string $senha): int {
+        try {
+            $stmtCheck = $this->conn->prepare("SELECT * FROM $this->table1 WHERE email = :email");
+            $stmtCheck->bindValue(":email", $email);
+            $stmtCheck->execute();
+
+            if ($stmtCheck->rowCount() > 0) {
+                return 3;
+            }
+
+            $hash = password_hash($senha, PASSWORD_DEFAULT);
+            $stmtCheck = $this->conn->prepare("INSERT INTO $this->table1 (nome, email, senha) VALUES (:nome, :email, :senha)");
+            $stmtCheck->bindValue(":nome", $nome);
+            $stmtCheck->bindValue(":email", $email);
+            $stmtCheck->bindValue(":senha", $hash);
+
+            if(!$stmtCheck->execute()){
+                return 2;
+            }
+
+            return 1;
+        } catch (Exception $e) {
+            error_log("Erro ao cadastrar Usuário: " . $e->getMessage());
+            return 0;
+        }
+    }
+
+    public function login(string $email, string $senha): int {
+        try {
+            $stmtCheck = $this->conn->prepare("SELECT * FROM $this->table1 WHERE email = :email");
+            $stmtCheck->bindValue(":email", $email);
+            $stmtCheck->execute();
+
+            $user = $stmtCheck->fetch(PDO::FETCH_ASSOC);
+
+            if ($stmtCheck->rowCount() == 0) {
+                return 3;
+            }
+
+            if (password_verify($senha, $user['senha'])) {
+                return 3;
+            }
+
+            return 1;
+
+        } catch (Exception $e) {
+            error_log("Erro ao fazer login: " . $e->getMessage());
+            return 0;
+        }
+    } 
+
+}
